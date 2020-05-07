2ff10aa64dd36f1df833e39f1768795bd4d26455
gr0mph <danoupro@gmail.com>
2020-05-08 00:31:40 +0200

    Add pacman class

--- SpringChallenge2020.py
+++ SpringChallenge2020.py
@@ -2,9 +2,14 @@ import sys
 import math
 import copy
 import random
+import numpy as np
+
+pacman_board = {}
 
 WIDTH, HEIGHT, MINE, OPP = 0, 0, 1, 0
 PACMAN_MAP = []
+
+TYPE_SET = { 'NEUTRAL' : 0 }
 
 def read_map():
     global WIDTH, HEIGHT
@@ -17,7 +22,21 @@ def t_check_map(PACMAN_MAP):
     for i in range(HEIGHT):
         print(PACMAN_MAP[i],file=sys.stderr)
 
-# Grab the pellets as fast as you can!
+class Pacman():
+
+    def __init__(self,clone):
+        self.id, self.mine = -1, -1
+        self.out = ''
+        self.state = None
+        if clone is not None :
+            self.id, self.mine = clone.id, clone.mine
+            self.state = copy.copy(clone.state)
+
+    def update(self,state):
+        self.state = [int(i) for i in state]
+
+    def write_move(self):
+        t = f'MOVE {self.id} {str(state[0])} {str(state[1])}'
 
 
 if __name__ == '__main__':
@@ -28,20 +47,22 @@ if __name__ == '__main__':
         my_score, opponent_score = [int(i) for i in input().split()]
         visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
         for i in range(visible_pac_count):
-            # pac_id: pac number (unique within a team)
-            # mine: true if this pac is yours
-            # x: position in the grid
-            # y: position in the grid
-            # type_id: unused in wood leagues
-            # speed_turns_left: unused in wood leagues
-            # ability_cooldown: unused in wood leagues
-            pac_id, mine, x, y, type_id, speed_turns_left, ability_cooldown = input().split()
-            pac_id = int(pac_id)
-            mine = mine != "0"
-            x = int(x)
-            y = int(y)
-            speed_turns_left = int(speed_turns_left)
-            ability_cooldown = int(ability_cooldown)
+            #state_in = [int(i) for i in input().split()]
+            state_in = input().split()
+            state_in[4] = TYPE_SET[state_in[4]]
+
+            for t in state_in :
+                print(t,file=sys.stderr)
+            pacman_id, mine = state_in[0], state_in[1]
+            if pacman_id in pacman_board :
+                pacman_board[pacman_id].update(state_in[2:])
+
+            else :
+                pacman_new = Pacman(None)
+                pacman_new.id, pacman_new.mine = pacman_id, mine
+                pacman_board[pacman_id] = pacman_new
+                pacman_board[pacman_id].update(state_in[2:])
+
         visible_pellet_count = int(input())  # all pellets in sight
         for i in range(visible_pellet_count):
             # value: amount of points this pellet is worth
