0bc191f1f4bd593886f4e56d0674d0fdfabb96c4
gr0mph <danoupro@gmail.com>
2020-05-07 23:16:58 +0200

    Add first read

--- SpringChallenge2020.py
+++ SpringChallenge2020.py
@@ -1,40 +1,54 @@
 import sys
 import math
+import copy
+import random
+
+WIDTH, HEIGHT, MINE, OPP = 0, 0, 1, 0
+PACMAN_MAP = []
+
+def read_map():
+    global WIDTH, HEIGHT
+    WIDTH, HEIGHT = [int(i) for i in input().split()]
+    global PACMAN_MAP
+    for i in range(HEIGHT):
+        PACMAN_MAP.append(list(input()))
+
+def t_check_map(PACMAN_MAP):
+    for i in range(HEIGHT):
+        print(PACMAN_MAP[i],file=sys.stderr)
 
 # Grab the pellets as fast as you can!
 
-# width: size of the grid
-# height: top left corner is (x=0, y=0)
-width, height = [int(i) for i in input().split()]
-for i in range(height):
-    row = input()  # one line of the grid: space " " is floor, pound "#" is wall
 
-# game loop
-while True:
-    my_score, opponent_score = [int(i) for i in input().split()]
-    visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
-    for i in range(visible_pac_count):
-        # pac_id: pac number (unique within a team)
-        # mine: true if this pac is yours
-        # x: position in the grid
-        # y: position in the grid
-        # type_id: unused in wood leagues
-        # speed_turns_left: unused in wood leagues
-        # ability_cooldown: unused in wood leagues
-        pac_id, mine, x, y, type_id, speed_turns_left, ability_cooldown = input().split()
-        pac_id = int(pac_id)
-        mine = mine != "0"
-        x = int(x)
-        y = int(y)
-        speed_turns_left = int(speed_turns_left)
-        ability_cooldown = int(ability_cooldown)
-    visible_pellet_count = int(input())  # all pellets in sight
-    for i in range(visible_pellet_count):
-        # value: amount of points this pellet is worth
-        x, y, value = [int(j) for j in input().split()]
+if __name__ == '__main__':
+    read_map()
+    t_check_map(PACMAN_MAP)
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
+    while True:
+        my_score, opponent_score = [int(i) for i in input().split()]
+        visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
+        for i in range(visible_pac_count):
+            # pac_id: pac number (unique within a team)
+            # mine: true if this pac is yours
+            # x: position in the grid
+            # y: position in the grid
+            # type_id: unused in wood leagues
+            # speed_turns_left: unused in wood leagues
+            # ability_cooldown: unused in wood leagues
+            pac_id, mine, x, y, type_id, speed_turns_left, ability_cooldown = input().split()
+            pac_id = int(pac_id)
+            mine = mine != "0"
+            x = int(x)
+            y = int(y)
+            speed_turns_left = int(speed_turns_left)
+            ability_cooldown = int(ability_cooldown)
+        visible_pellet_count = int(input())  # all pellets in sight
+        for i in range(visible_pellet_count):
+            # value: amount of points this pellet is worth
+            x, y, value = [int(j) for j in input().split()]
 
-    # MOVE <pacId> <x> <y>
-    print("MOVE 0 15 10")
+        # Write an action using print
+        # To debug: print("Debug messages...", file=sys.stderr)
+
+        # MOVE <pacId> <x> <y>
+        print("MOVE 0 15 10")
